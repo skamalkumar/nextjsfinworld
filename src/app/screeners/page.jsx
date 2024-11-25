3ea0045230a305d2021a391700a57be32f3d2100
dheeraj2096 <dheerajreddy571@gmail.com>
2024-11-25 14:34:19 +0000

    first commit

--- src/app/screeners/page.jsx
+++ src/app/screeners/page.jsx
@@ -1,19 +1,19 @@
-// pages/blog.js or any other page
-import { usePosts } from '../hooks/usePosts';
+// // pages/blog.js or any other page
+// import { usePosts } from '../hooks/usePosts';
 
-export default function BlogPage() {
-  const { posts, loading } = usePosts();
+// export default function BlogPage() {
+//   const { posts, loading } = usePosts();
 
-  if (loading) return <div>Loading...</div>;
+//   if (loading) return <div>Loading...</div>;
 
-  return (
-    <div>
-      {posts.map(post => (
-        <article key={post.id}>
-          <h2>{post.title}</h2>
-          <p>{post.content}</p>
-        </article>
-      ))}
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       {posts.map(post => (
+//         <article key={post.id}>
+//           <h2>{post.title}</h2>
+//           <p>{post.content}</p>
+//         </article>
+//       ))}
+//     </div>
+//   );
+// }
