a96085198fa9fde5b9255c36148f50dd96a2a511
dheeraj2096 <dheerajreddy571@gmail.com>
2024-11-25 17:46:30 +0000

    first commit

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -85,7 +85,7 @@ function Homepage() {
         <section className="bg-gradient-to-r from-blue-500 to-green-500 text-white py-20 flex items-center justify-center rounded-lg mx-4 my-2 shadow-lg">
           <div className="container mx-auto">
             <h2 className="text-3xl font-bold mb-8 text-gray-800 text-center">
-              Latest Videos
+              Latest Stock Market Videos
             </h2>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
               {videos.map((video) => (
