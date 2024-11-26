9e0ac32be11ba9d3c5555c124d3862267088af6f
dheeraj2096 <dheerajreddy571@gmail.com>
2024-11-26 11:23:31 +0000

    first commit

--- src/app/contactus/page.jsx
+++ src/app/contactus/page.jsx
@@ -88,9 +88,15 @@ const handleSubmit = async (e) => {
     <div className="grid grid-cols-1 md:grid-cols-3">
       {/* Shahdol Address Section */}
       <div className="p-8 bg-gray-50">
-        <h3 className="text-2xl font-bold text-gray-700 mb-4 text-center">
-          Shahdol Office
-        </h3>
+      <h3 className="text-2xl font-bold text-gray-700 mb-4 text-center flex items-center justify-center">
+    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="20" className="mr-2">
+        <rect width="30" height="20" fill="#FF9933"/>
+        <rect width="30" height="6.667" y="6.667" fill="white"/>
+        <rect width="30" height="6.667" y="13.333" fill="#138808"/>
+        <circle cx="15" cy="10" r="3.333" fill="#000080"/>
+    </svg>
+    Indian Office
+</h3>
         <p className="text-gray-600 text-center text-lg leading-relaxed mb-6">
           Ward No 28, Behind Christian Hospital, Burhar Road, Shahdol, MP 484001
         </p>
@@ -102,7 +108,7 @@ const handleSubmit = async (e) => {
             style={{ border: 0 }}
             allowFullScreen=""
             loading="lazy"
-            title="Shahdol Office Map"
+            title="Indian Office Map"
             referrerPolicy="no-referrer-when-downgrade"
           ></iframe>
         </div>
@@ -185,8 +191,21 @@ const handleSubmit = async (e) => {
 
       {/* London Address Section */}
       <div className="p-8 bg-gray-50">
-        <h3 className="text-2xl font-bold text-gray-700 mb-4 text-center">
-          London Office
+      <h3 className="text-2xl font-bold text-gray-700 mb-4 text-center flex items-center justify-center">
+    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="20" viewBox="0 0 60 30" className="mr-2">
+        <clipPath id="s">
+            <path d="M0,0 v30 h60 v-30 z"/>
+        </clipPath>
+        <clipPath id="t">
+            <path d="M30,15 h30 v15 z v15 h-30 z h-30 v-15 z v-15 h30 z"/>
+        </clipPath>
+        <g clipPath="url(#s)">
+            <path d="M0,0 v30 h60 v-30 z" fill="#012169"/>
+            <path d="M0,0 L60,30 M60,0 L0,30" stroke="#fff" strokeWidth="6"/>
+            <path d="M0,0 L60,30 M60,0 L0,30" clipPath="url(#t)" stroke="#C8102E" strokeWidth="4"/>
+        </g>
+        </svg>
+         UK Office
         </h3>
         <p className="text-gray-600 text-center text-lg leading-relaxed mb-6">
           3rd Floor, 86-90, Paul Street, London, England, United Kingdom,
