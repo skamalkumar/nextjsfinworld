b2d7b2a22a14771f0cde94d320e68cf7d4115742
dheeraj2096 <dheerajreddy571@gmail.com>
2024-12-03 22:06:04 +0530

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import localFont from "next/font/local";
+import Script from "next/script"; // Import Script for adding analytics
 import "./globals.css";
 import { AuthProvider } from "../../context/AuthContext";
 import Header from "@/components/Header";
@@ -17,15 +18,29 @@ const geistMono = localFont({
 
 export const metadata = {
   title: "FinWorld",
-  description: "A modern financial platform",
+  description: "We specialize in investment planning and insurance management, ensuring that your financial portfolio is not only optimized for growth but also safeguarded against uncertainties. From selecting the right investment avenues to securing your future with personalized insurance solutions, FinWorld offers a holistic approach to wealth management",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        {/* Add the favicon here */}
+        {/* Add the favicon */}
         <link rel="icon" href="/finworld-logo.webp" />
+
+        {/* Google Analytics Script */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-FWZJE5ELJC" // Replace G-XXXXXXXXXX with your Measurement ID
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-FWZJE5ELJC'); // Replace G-XXXXXXXXXX with your Measurement ID
+          `}
+        </Script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
