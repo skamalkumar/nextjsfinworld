118d6f70fb8e8cb84d4be9fe060e2f01a1eb6b15
dheeraj2096 <dheerajreddy571@gmail.com>
2024-12-04 12:17:40 +0530

    first commit

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    siteUrl: 'https://finworldltd.online', // Replace with your website's domain
+    generateRobotsTxt: true, // Generate a robots.txt file
+    exclude: ['/404'], // Exclude specific pages (optional)
+    changefreq: 'daily', // Frequency of changes
+    priority: 0.7, // Default priority for pages
+  };
+  
