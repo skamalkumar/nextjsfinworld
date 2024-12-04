4860a50ea3d8ea77795277cd9ccd24542f8b2717
dheeraj2096 <dheerajreddy571@gmail.com>
2024-12-04 13:06:14 +0530

    first commit

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,7 @@
-module.exports = {
-    siteUrl: 'https://finworldltd.online', // Replace with your website's domain
-    generateRobotsTxt: true, // Generate a robots.txt file
-    exclude: ['/404'], // Exclude specific pages (optional)
-    changefreq: 'daily', // Frequency of changes
-    priority: 0.7, // Default priority for pages
-  };
-  
+export default {
+  siteUrl: 'https://finworldltd.online', // Replace with your website's domain
+  generateRobotsTxt: true, // Generate a robots.txt file
+  exclude: ['/404'], // Exclude specific pages (optional)
+  changefreq: 'daily', // Frequency of changes
+  priority: 0.7, // Default priority for pages
+};
