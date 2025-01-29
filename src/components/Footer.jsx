e8f8a37f440aa69d864dd0f46663be0f8f41c983
kamal kumar <skamalkumar@gmail.com>
2025-01-29 15:55:43 +0530

    message

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,14 +3,14 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="flex justify-between items-center p-6 bg-blue-500 text-white rounded-md mx-4 my-2">
-      <div className="container mx-auto flex justify-center items-center">
-        {/* Footer content */}
-        <p className="text-center">
-          &copy; 2024 FinWorld. All rights reserved.
-        </p>
-      </div>
-    </footer>
+    <footer className="flex flex-col justify-center items-center p-6 bg-blue-500 text-white rounded-md mx-4 my-2 text-center">
+  <div className="container mx-auto">
+    <p>&copy; 2024 FinWorld. All rights reserved.</p>
+    <p className="mt-2 text-sm">
+      SEBI Registration No: INZ000163138 | BSE Registration No: AP01668101166717 | NSE Registration No: AP3022002931 | AMFI Registration No: ARN-102132
+    </p>
+  </div>
+</footer>
   );
 }
 
