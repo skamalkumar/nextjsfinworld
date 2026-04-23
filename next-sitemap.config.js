91cd20a989c2d5f1e38ea229fe20ca6e9ccab073
Kamalr <skamalkumar@gmail.com>
2026-04-23 22:13:57 +0530

    fix blog articles1

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,28 +1,31 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://finworldltd.online',
+  siteUrl: 'https://www.finworldltd.online',
   generateRobotsTxt: true,
 
   changefreq: 'daily',
   priority: 0.7,
 
-  exclude: ['/404'],
-
   additionalPaths: async (config) => {
-    const res = await fetch(
-      'https://api.github.com/repos/skamalkumar/finworldarticles/contents/content/articles'
-    );
+    try {
+      const res = await fetch(
+        'https://api.github.com/repos/skamalkumar/finworldarticles/contents/content/articles?ref=main'
+      );
 
-    const data = await res.json();
+      const data = await res.json();
 
-    const blogPaths = data.map((file) => ({
-      loc: `/blog/${file.name.replace('.html', '')}`,
-      changefreq: 'daily',
-      priority: 0.8,
-    }));
+      const blogPaths = data
+        .filter((file) => file.name.endsWith('.html'))
+        .map((file) => ({
+          loc: `/blog/${file.name.replace('.html', '')}`,
+          changefreq: 'daily',
+          priority: 0.8,
+        }));
 
-    return [
-      ...blogPaths,
-    ];
+      return blogPaths;
+    } catch (err) {
+      console.error("Sitemap error:", err);
+      return [];
+    }
   },
 };
