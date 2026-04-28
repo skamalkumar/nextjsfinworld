e5d32b466872cfc75629c1616547667561c70da7
Kamalr <skamalkumar@gmail.com>
2026-04-28 21:28:51 +0530

    slug

--- src/app/blog/[slug]/page.js
+++ src/app/blog/[slug]/page.js
@@ -16,18 +16,16 @@ export default async function BlogPost({ params }) {
     const content = await res.text();
 
     return (
-      <article className="max-w-4xl mx-auto p-6 bg-blue-100 rounded-lg shadow-md">
-        <h1 className="text-3xl font-bold mb-4">{slug.replaceAll('-', ' ')}</h1>
-
+      <div className="min-h-screen">
         <div
-          className="prose max-w-none"
           dangerouslySetInnerHTML={{ __html: content }}
         />
-
-        <a href="/blog" className="text-blue-500 mt-6 inline-block">
-          ← Back to Blog
-        </a>
-      </article>
+        <div className="max-w-4xl mx-auto px-6 pb-8">
+          <a href="/blog" className="text-blue-400 inline-block">
+            ← Back to Blog
+          </a>
+        </div>
+      </div>
     );
   } catch (error) {
     return <div>Error loading post</div>;
